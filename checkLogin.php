ae820032c4ebbc1c91125c3051c811349deac230
fatwinnie <49060668+fatwinnie@users.noreply.github.com>
2020-07-28 07:30:30 +0800

    Update checkLogin.php

--- checkLogin.php
+++ checkLogin.php
@@ -18,14 +18,21 @@ $result = mysqli_query($connect,"SELECT * FROM user where userName='$id'");
 $row = mysqli_fetch_assoc($result);
 if($id!=null && $pw!=null && $row['userName']==$id && $row['password']==$pw){
     $rank = mysqli_query($connect,"SELECT user_rank FROM user where userName='$id' and password='$pw'");
+    $rank_row = mysqli_fetch_assoc($rank);
+    $rank_result = $rank_row['user_rank'];
+    //print_r($rank_row) ;
+    //echo $rant_result ;
     session_start(); 
     $_SESSION['userName'] = $id;   
-    $_SESSION['user_rank'] = $rank;
+    $_SESSION['user_rank'] = $rank_result;
+    //echo $_SESSION['userName'];
+    //echo $_SESSION['user_rank'];
     //echo '登入成功!';
     
     switch($_SESSION['user_rank']){
         case 1:
-            header("location: memList.php");
+            echo '<meta http-equiv=REFRESH CONTENT=1;url=memList.php>';
+            //header("location: memList.php");
             break;
         case 2:
             header("location: productList.php");
@@ -33,10 +40,10 @@ if($id!=null && $pw!=null && $row['userName']==$id && $row['password']==$pw){
         case 3:
             header("location: adduser.php");
             break;
-          //default:
-           // header("location: login.php");
-           // break;
-        }
+        default:
+            header("location: test.php");
+            break;
+        } 
 
     }
     //$rank = mysqli_query($connect,"SELECT user_rank FROM user where userName='$id' and password='$pw'");
